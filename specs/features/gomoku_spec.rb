b18f47a33b4abb602a3b7774003f963dfdc7e7ea
Steven Wang <3460883@gmail.com>
2017-07-25 20:18:12 -0700

    Specify base spec of the gomoku game
    Players were not intitally thought of when designing the interface. They
    could be introduced later, and just pretend the information was unknown
    at the point. The cost of doing so was modifying the core path.
    
    On second thought, the core path would not be complete without the idea
    of players. MVP should not mean being spearheaded and only focusing on one
    tiniest single important thing at a time.

--- specs/features/gomoku_spec.rb
+++ specs/features/gomoku_spec.rb
@@ -1,3 +1,15 @@
 describe "gomoku" do
-  it "is won by placing 5 pices of the same kind in a straight line on the board"
+  it "is won by connecting 5 game pieces in a row on a 2-D plane by the same team" do
+    pending
+    gomoku = Gomoku.start
+    gomoku.place row: 2, column: 9, is_host: false
+    gomoku.place row: 3, column: 8, is_host: false
+    gomoku.place row: 4, column: 7, is_host: false
+    gomoku.place row: 5, column: 6, is_host: false
+
+    expect(gomoku.to_receive :is_won)
+
+    gomoku.place row: 6, column: 5, is_host: false
+    expect(gomoku.winner).to eq :guest
+  end
 end
